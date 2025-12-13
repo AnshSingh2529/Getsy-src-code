b5ad37b1d09ebb27b5e7cc7cda67d504a815fa62
Getasy <getasy.in@gmail.com>
2025-12-14 02:38:57 +0530

    Add UserAdmin configuration to Django admin for user management

--- server/apps/user_auth/admin.py
+++ server/apps/user_auth/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "name",
+        "role",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "email",
+        "name",
+        "phone",
+    )
+
+    ordering = ("-date_joined",)
+
+    readonly_fields = ("last_login", "date_joined")
