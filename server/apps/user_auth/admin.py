cf19ef326b42305eca0a524c4c543bebd3950801
Getasy <getasy.in@gmail.com>
2025-12-27 17:07:59 +0530

    Register UserAdmin in admin.py

--- server/apps/user_auth/admin.py
+++ server/apps/user_auth/admin.py
@@ -1,5 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom admin for email-based authentication.
+    """
+
+    ordering = ("email",)
+    list_display = ("id", "email", "username", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email", "username")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("username",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+    filter_horizontal = ("groups", "user_permissions")
