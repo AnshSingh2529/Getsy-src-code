f8aa917e31d44a4dbd65ef0d45cc0bebbc7a733f
Getasy <getasy.in@gmail.com>
2025-12-28 15:07:27 +0530

    Added Profle, Agency, Dealer Views Urls

--- server/apps/user_auth/urls.py
+++ server/apps/user_auth/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("token/", views.get_jwt_token, name="get_token"),
+    path("me/", ProfileView.as_view()),
+    path("dealer/dashboard/", DealerDashboardView.as_view()),
+    path("agency/dashboard/", AgencyDashboardView.as_view()),
 ]
