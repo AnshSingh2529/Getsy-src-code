e76d4b59c7f8cea793b290713e95a02088bf776f
Getasy <getasy.in@gmail.com>
2025-12-28 15:37:02 +0530

    Added Agency and Dealer Views and improvement in views

--- server/apps/user_auth/views.py
+++ server/apps/user_auth/views.py
@@ -2,6 +2,9 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from apps.user_auth.permissions import IsAgency, IsDealer, IsEndUser
 
 
 @api_view(["GET"])
@@ -26,3 +29,42 @@ def get_jwt_token(request):
     )
 
 
+class ProfileView(APIView):
+    permission_classes = [IsEndUser]
+
+    def get(self, request):
+        user = request.user
+        return Response(
+            {
+                "id": user.id,
+                "email": user.email,
+                "username": user.username,
+                "role": user.role,
+            }
+        )
+
+
+class DealerDashboardView(APIView):
+    permission_classes = [IsDealer]
+
+    def get(self, request):
+        return Response(
+            {
+                "message": "Dealer dashboard access granted",
+                "email": request.user.email,
+                "role": request.user.role,
+            }
+        )
+
+
+class AgencyDashboardView(APIView):
+    permission_classes = [IsAgency]
+
+    def get(self, request):
+        return Response(
+            {
+                "message": "Agency level access",
+                "email": request.user.email,
+                "role": request.user.role,
+            }
+        )
