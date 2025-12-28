e29f2dc70b68b7640b9de840594a32accc8da5a8
Getasy <getasy.in@gmail.com>
2025-12-28 14:44:46 +0530

    Added RoleChoices as Agency | Dealer | User to differentiate User Base

--- server/apps/user_auth/models.py
+++ server/apps/user_auth/models.py
@@ -3,7 +3,19 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    class RoleChoices(models.TextChoices):
+        USER = "user", "User"
+        Agency = "agency", "Agency"
+        Dealer = "dealer", "Dealer"
 
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=RoleChoices.choices,
+        default=RoleChoices.USER,
+    )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
