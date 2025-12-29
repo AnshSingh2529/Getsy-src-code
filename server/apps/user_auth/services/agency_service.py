6ccfdb549c5a44b53f3ea0c3bb306d700a6ceb9b
Getasy <getasy.in@gmail.com>
2025-12-30 02:25:38 +0530

    added some presave services and user role pupulation after creation of Agencies

---
+++ server/apps/user_auth/services/agency_service.py
@@ -0,0 +1,28 @@
+from ..models import Agency, AgencyAddress, User
+from django.db import transaction
+
+
+@transaction.atomic
+def create_agency_with_role_address(*, agency_data, address_data, user: User):
+    """
+    Single source of truth for Agency creation
+    """
+    if user.role != User.RoleChoices.USER:
+        raise ValueError("User already has an assigned role")
+
+    if hasattr(user, "agency"):
+        raise ValueError("User already owns an agency")
+
+    agency = Agency(
+        owner=user,
+        **agency_data,
+    )
+    AgencyAddress.objects.create(
+        agency=agency,
+        **address_data,
+    )
+
+    user.role = User.RoleChoices.AGENCY
+    user.save(update_fields=["role"])
+
+    return agency
