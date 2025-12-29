76c4d79d1c783f0f300969006436061186d61c25
Getasy <getasy.in@gmail.com>
2025-12-30 02:51:40 +0530

    move REST_FRAMEWORK config of authentication class

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -83,8 +83,8 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
