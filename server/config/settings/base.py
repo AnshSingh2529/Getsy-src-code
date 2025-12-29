a7aee1cc93d4b918a904244bd984df7bbd11914d
Getasy <getasy.in@gmail.com>
2025-12-30 03:29:21 +0530

    added simple_jwt configs

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -89,6 +89,29 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "ISSUER": "getsy-api",
+    "AUDIENCE": None,
+    "SLIDING_TOKEN_LIFETIME": None,
+    "SLIDING_TOKEN_REFRESH_LIFETIME": None,
+    "LEEWAY": 0,
+}
+
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
