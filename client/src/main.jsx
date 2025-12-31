9314c6d6d8993f9021ee43e91e93d390f9c2a413
Getasy <getasy.in@gmail.com>
2025-12-31 21:56:35 +0530

    import AuthGate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./main.css";
 import { Provider } from "react-redux";
+// local imports...
 import { store } from "./store/store.js";
 import { router } from "./app/router.jsx";
-import AuthGate from "./components/AuthGate.jsx";
+import AuthGate from "./services/auth/AuthGate.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
