0a7eb1a3ac21cc9da48d3e002d3294157d4f0b0c
Getasy <getasy.in@gmail.com>
2026-01-01 16:19:12 +0530

    Modified the spinner

--- client/src/utils/LoadingSpinner.jsx
+++ client/src/utils/LoadingSpinner.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
+import { ActivityIcon } from "lucide-react";
 
 const LoadingSpinner = () => {
-    return (
-        <div className='min-h-screen bg-gradient-to-br from-gray-700 via-gray-900 to bg-gray-900 flex items-center justify-center relative overflow-hidden '>
-            <motion.div
-                className='w-16 h-16 border-4  border-t-4 border-t-black rounded-full'
-                animate={{ rotate: 360}}
-                transition={{duration:1, repeat: Infinity, ease: 'linear'}}
-            />
-        </div>
-    )
-}
+  return (
+    <div className=" flex items-center justify-center relative overflow-hidden">
+      <motion.div
+        className="outine-none flex items-center justify-center w-8 h-8 border-4  border-t-4 border-t-transparent border-b-4 border-b-transparent rounded-full"
+        animate={{ rotate: 360, scale: [1, 1.05, 1] }}
+        transition={{
+          rotate: { repeat: Infinity, duration: 1, ease: "linear" },
+          scale: { repeat: Infinity, duration: 1.2, ease: "easeInOut" },
+        }}
+      >
+        <ActivityIcon />
+      </motion.div>
+    </div>
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
