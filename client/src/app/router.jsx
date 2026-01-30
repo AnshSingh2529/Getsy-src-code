e8adcb02e53cd3387bb5e65df0d25f9ec40d9255
Getasy <getasy.in@gmail.com>
2026-01-30 17:28:59 +0530

    Refactor router configuration; comment out unused Home import, update Home route to use EnhancedHomePage, and add AgencyRegistration route

--- client/src/app/router.jsx
+++ client/src/app/router.jsx
@@ -10,7 +10,7 @@ import { ProtectedRoutes } from "../routes/ProtectedRoutes.jsx";
 // import from layouts...
 import Layout from "../layouts/Layout.jsx";
 // import from pages...
-import Home from "../pages/Home.jsx";
+// import Home from "../pages/Home.jsx";
 import PostProperty from "../pages/postProperty/PostProperty.jsx";
 import HireDealerAgenciesModal from "../pages/AgencyDealerPropertyPage/HireDealerAgenciesModal.jsx";
 import LoginPage from "../pages/authentication/LoginPage.jsx";
@@ -18,11 +18,13 @@ import CommonView from "../pages/propertyPages/CommonView.jsx";
 import ForCouples from "../pages/propertyPages/ThroughAgents/ForCouples.jsx";
 import ForFamilies from "../pages/propertyPages/ThroughAgents/ForFamilies.jsx";
 import UserProfile from "../pages/UserProfile.jsx";
-import  Getsy404Page  from "../pages/Getsy404Page.jsx";
+import Getsy404Page from "../pages/Getsy404Page.jsx";
 import DealersForBachelors from "../pages/propertyPages/ThroughDealers/DealersForBachelors.jsx";
 // import form dashboard...
 import AgencyDashboard from "../dashboards/Agents/AgencyDashboard.jsx";
 import DealersDashboard from "../dashboards/Dealers/DealersDashboard.jsx";
+import AgencyRegistration from "../pages/NewRegistrationPage/AgencyRegistration.jsx";
+import HomePage from "../components/main/EnhancedHomePage.jsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,12 +33,10 @@ export const router = createBrowserRouter(
         {/* PUBLIC_ROUTES */}
 
         {/* page-routes */}
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route
-          path="top-agencies"
-          element={<HireDealerAgenciesModal />}
-        />
+        {/* <Route index element={<Home />} /> */}
+        <Route index element={<HomePage />} />
+        <Route path="top-agencies" element={<HireDealerAgenciesModal />} />
+
         <Route
           element={<ProtectedRoutes allowedRoles={["agency", "dealer"]} />}
         >
@@ -83,7 +83,8 @@ export const router = createBrowserRouter(
         </Route>
 
         <Route path="*" element={<Getsy404Page />} />
+        <Route path="/register-your-firm" element={<AgencyRegistration />} />
       </Route>
-    </>
-  )
+    </>,
+  ),
 );
